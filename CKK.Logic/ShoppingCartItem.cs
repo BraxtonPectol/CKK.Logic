a2a4078da7d4fb927bffc6c19441f4483603edf8
Braxton Pectol <BraxxtotheMaxx>
2023-04-27 10:21:28 -0600

    Easy Peasy

---
+++ CKK.Logic/ShoppingCartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic
+{
+    internal class ShoppingCartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public ShoppingCartItem (Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ CKK.Logic/StoreItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic
+{
+    internal class StoreItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public StoreItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
