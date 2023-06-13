59f6107b57ac87ea398bbfa77d63edc3d4115b99
BraxxtotheMaxx <BraxxToTheMaxx@gmail.com>
2023-06-12 22:50:19 -0600

    All done

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -4,9 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CKK.Logic
+namespace CKK.Logic.Models
 {
-    internal class ShoppingCart
+    public class ShoppingCart
     {
+        private Customer Customer;
+        private ShoppingCartItem Product1;
+        private ShoppingCartItem Product2;
+        private ShoppingCartItem Product3;
+
+        public ShoppingCart(Customer cust)
+        {
+            Customer = cust;
+        }
+        public int GetCutomerid()
+        {
+            return Customer.GetId();
+        }
+        public ShoppingCartItem GetProductByld(int id)
+        {
+            if (id == 1)
+            {
+                return Product1;
+            }
+            if (id == 2)
+            {
+                return Product2;
+            }
+            if (id == 3)
+            {
+                return Product3;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public ShoppingCartItem AddProduct(Product prod)
+        {
+            AddProduct(prod, 1);
+        }
+        public ShoppingCartItem AddProduct(Product prod, int quantity)
+        {
+            if (quantity >= 1) 
+            {
+                if (Product1.GetProduct == prod.GetName)
+                {
+                    Product1.SetQuantity(quantity + (Product1.GetQuantity));
+                }
+            }
+        }
     }
 }
